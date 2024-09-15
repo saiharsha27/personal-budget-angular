90e04e69d0eb8d443768e7c4d0d9f825ce7055d3
saiharsha paladugu <saiharshapaladugu@saiharshas-MBP.lan>
2024-09-15 19:45:05 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,36 +1,24 @@
 const express = require('express');
-const { title } = require('process');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Groceries',
-        budget: 110
-    },
-    ]
-};
-
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Server Error');
+            return;
+        }
+        res.json(JSON.parse(data));
+    });
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
